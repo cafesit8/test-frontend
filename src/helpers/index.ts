596b295b12fb93b5071869ae2a2b3b3cb142bee1
leliasgomer <leliasgomer@gmail.com>
2025-03-28 18:43:43 -0500

    ✨ add verify date helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,13 @@
 export function convertDateToTimestamp(fechaString: string) {
   return new Date(fechaString);
 }
+
+export function verifyDate(date: Date) {
+  const dateString = String(date)
+  const isValid = dateString.includes('T')
+
+  if (isValid) {
+    return dateString.split('T')[0]
+  }
+  return dateString
+}
