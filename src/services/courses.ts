5db4722c8ddeb2b1a5f7ffddfd9932320803c062
leliasgomer <leliasgomer@gmail.com>
2025-03-28 17:57:13 -0500

    ✨ add delete and update method

--- src/services/courses.ts
+++ src/services/courses.ts
@@ -33,3 +33,39 @@ export async function addCourse(course: Course) {
     throw new Error('Network response was not ok')
   }
 }
+
+export async function deleteCourse(id: number) {
+  try {
+    const response = await fetch(`${URL_COURSES}/${id}`, {
+      method: 'DELETE'
+    })
+  
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    return response.json()
+  } catch (error) {
+    throw new Error('Network response was not ok')
+  }
+}
+
+export async function updateCourse(course: Course) {
+  try {
+    const response = await fetch(`${URL_COURSES}/${course.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+    })
+  
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    return response.json()  
+  } catch (error) {
+    throw new Error('Network response was not ok')  
+  }
+}
